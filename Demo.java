d7e4fe0636c797393945e02350530a3e6c7c616a
UmaAchuthanMaya <248765@ust.com>
2023-07-26 10:37:35 +0530

    10:37

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
-public class Demo(){
- public static voin main(String[]args){
-	System.out.println("hellooooo");
- }
-}
+public class Demo{
+
+public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
